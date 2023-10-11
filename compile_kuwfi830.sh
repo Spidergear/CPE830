2a674dbf77d6c8ef26a9a006c462eb50972b7b40
Spidergear <34096834+Spidergear@users.noreply.github.com>
2023-10-11 15:03:03 +0200

    Update compile_kuwfi830.sh

--- compile_kuwfi830.sh
+++ compile_kuwfi830.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-rm -rf CPE830
-git clone https://git.openwrt.org/openwrt/openwrt.git CPE830
+rm -rf openwrt
+git clone https://git.openwrt.org/openwrt/openwrt.git openwrt
 rm 3133.patch
-wget https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3133.patch
-cp config_CPE830 CPE830/.config
-cd CPE830
+wget https://github.com/Spidergear/CPE830/blob/master/3133.patch
+cp config_ikeja openwrt/.config
+cd openwrt
 patch -p1 < ../3133.patch
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cd ..
-cp config_CPE830 CPE830/.config
-chown -R admin:admin CPE830
-cd CPE830
-sudo -u admin make defconfig
-sudo -u admin make -j16 download
-sudo -u admin make -j16
-cd ..
-gzip -9 CPE830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin
-mv CPE830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin.gz WHEREYOUWANT
+cp config_ikeja openwrt/.config
+#chown -R admin:admin kuwfi830
+cd kuwfi830
+make defconfig
+make -j16 download
+make -j16 V=s
+#cd ..
+#gzip -9 kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin
+#mv kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin.gz WHEREYOUWANT
+
